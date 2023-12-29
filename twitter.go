57990420a2a89fed95f02428b8c013f608a505c7
I.Pastir <ivan@pastir.me>
2023-12-29 19:54:19 +0200

    v0.2

--- twitter.go
+++ twitter.go
@@ -1,15 +1,11 @@
 package gooauth
 
 import (
-	"context"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
-	"errors"
 	"fmt"
-	"io"
 	"math/rand"
-	"net/http"
 	"net/url"
 	"sort"
 	"strconv"
@@ -17,56 +13,64 @@ import (
 	"time"
 )
 
-type Twitter struct {
-	OauthConsumerKey     string
-	OauthConsumerSecret  string
-	OauthNonce           string
-	OauthSignatureMethod string
-	OauthTimestamp       string
-	OauthToken           string
-	OauthTokenSecret     string
-	ApiUrl               string
-	OauthVersion         string
-	OauthSignature       string
-	OauthUrlCallback     string
+type (
+	Twitter struct {
+		OauthConsumerKey     string
+		OauthConsumerSecret  string
+		OauthNonce           string
+		OauthSignatureMethod string
+		OauthTimestamp       string
+		OauthToken           string
+		OauthTokenSecret     string
+		OauthVersion         string
+		OauthSignature       string
+		OauthUrlCallback     string
+		ApiUrl               string
+		Parameters           map[string]string
+		ParametersString     string
+		BaseString           strings.Builder
+		SigningKey           string
+		ResponseReqToken     ResponseRequestToken
+	}
 
-	OautHeader http.Header
+	// ResponseRequestToken oauth_token=FeBpvQAAAAABrc5sAAABjLE_WJw&oauth_token_secret=1fqLzSgl6RfeeLstAHQBuJQFVJcAkQNb&oauth_callback_confirmed=true
+	ResponseRequestToken struct {
+		OauthToken             string
+		OauthTokenSecret       string
+		OauthCallbackConfirmed string
+	}
+)
 
-	Params map[string]string
+func Oauth(accessToken string, tokenSecret string, consumerKey string, consumerSecret string, oauthVersion string, apiUrl string) (Twitter, error) {
+	var err error
+	tw := Twitter{
+		OauthToken:           accessToken,
+		OauthTokenSecret:     tokenSecret,
+		OauthConsumerKey:     consumerKey,
+		OauthConsumerSecret:  consumerSecret,
+		OauthVersion:         oauthVersion,
+		OauthSignatureMethod: "HMAC-SHA1",
+		OauthTimestamp:       strconv.FormatInt(time.Now().Unix(), 10),
+		ApiUrl:               apiUrl,
+	}
+
+	tw.Parameters = make(map[string]string)
+	tw.generateNonce()
+	tw.createCollectingParameters()
+	err = tw.createSignatureBaseString()
+	if err != nil {
+		return Twitter{}, err
+	}
+
+	err = tw.createSigningKey()
+	if err != nil {
+		return Twitter{}, err
+	}
+
+	return tw, nil
 }
 
-func (t *Twitter) Init(consumerKey string, accessToken string, consumerSecret string, tokenSecret string, oauthVersion string) error {
-	t.OauthConsumerKey = consumerKey
-	t.OauthConsumerSecret = consumerSecret
-	t.OauthSignatureMethod = "HMAC-SHA1"
-	t.OauthTimestamp = strconv.FormatInt(time.Now().Unix(), 10)
-	t.OauthToken = accessToken
-	t.OauthTokenSecret = tokenSecret
-	t.OauthVersion = oauthVersion
-	t.generateNonce()
-	t.Params = map[string]string{
-		"status":                 "Hello Twitter!",
-		"oauth_consumer_key":     t.OauthConsumerKey,
-		"oauth_nonce":            t.OauthNonce,
-		"oauth_signature_method": t.OauthSignatureMethod,
-		"oauth_timestamp":        t.OauthTimestamp,
-		"oauth_token":            accessToken,
-		"oauth_version":          oauthVersion,
-	}
-
-	err := t.generateOauthV1Signature()
-	if err != nil {
-		return err
-	}
-
-	err = t.createAuthHeader()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
+// generateNonce - generate nonce
 func (t *Twitter) generateNonce() {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -79,141 +83,105 @@ func (t *Twitter) generateNonce() {
 	t.OauthNonce = string(nonce)
 }
 
-// generateSignature https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature
-func (t *Twitter) generateOauthV1Signature() error {
+// collectingParameters Collecting parameters
+func (t *Twitter) createCollectingParameters() {
+	t.Parameters = map[string]string{
+		"oauth_consumer_key":     t.OauthConsumerKey,
+		"oauth_nonce":            t.OauthNonce,
+		"oauth_signature_method": t.OauthSignatureMethod,
+		"oauth_timestamp":        t.OauthTimestamp,
+		"oauth_version":          t.OauthVersion,
+		"oauth_token":            t.OauthToken,
+	}
+
 	// 1. Sort the list of parameters alphabetically [1] by encoded key [2].
 	var keys []string
-
-	for k := range t.Params {
+	for k := range t.Parameters {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 
-	var paramString strings.Builder
-	var baseString strings.Builder
-	var signingKey strings.Builder
+	var parameters strings.Builder
+	// Percent encode every key and value that will be signed.
+	// Append the ‘=’ character to the output string.
+	// If there are more key/value pairs remaining, append a ‘&’ character to the output string.
+	for _, v := range keys {
+		if v != "" {
+			parameters.WriteString(url.QueryEscape(v))
+			parameters.WriteString("=")
+			parameters.WriteString(url.QueryEscape(t.Parameters[v]))
+			parameters.WriteString("&")
+		}
+	}
+
+	// Cut last symbol &
+	t.ParametersString = strings.TrimRight(parameters.String(), "&")
+}
+
+// createSignatureBaseString - Creating the signature base string
+func (t *Twitter) createSignatureBaseString() error {
 
 	// Convert the HTTP Method to uppercase and set the output string equal to this value.
 	// Append the ‘&’ character to the output string.
-	_, err := baseString.WriteString("POST&")
+	_, err := t.BaseString.WriteString("POST&")
 	if err != nil {
 		return err
 	}
 	// Percent encode the URL and append it to the output string.
+	_, err = t.BaseString.WriteString(url.QueryEscape(t.ApiUrl))
+	if err != nil {
+		return err
+	}
 	// Append the ‘&’ character to the output string.
-	_, err = baseString.WriteString(url.QueryEscape(t.ApiUrl))
-	if err != nil {
-		return err
-	}
-	_, err = baseString.WriteString("&")
+	_, err = t.BaseString.WriteString("&")
 	if err != nil {
 		return err
 	}
 
-	// Append the encoded key to the output string.
-	// Append the ‘=’ character to the output string.
-	// Append the encoded value to the output string.
-	// If there are more key/value pairs remaining, append a ‘&’ character to the output string.
-	for _, k := range keys {
-		_, err = paramString.WriteString(url.QueryEscape(k))
-		if err != nil {
-			return err
-		}
-		_, err = paramString.WriteString("=")
-		if err != nil {
-			return err
-		}
-		_, err = paramString.WriteString(url.QueryEscape(t.Params[k]))
-		if err != nil {
-			return err
-		}
-	}
-
-	// The value which identifies your app to Twitter is called the consumer secret and can be found in the developer
-	// portal by viewing the app details page. This will be the same for every request your Twitter app sends.
-	_, err = signingKey.WriteString(url.QueryEscape(t.OauthConsumerSecret))
-	if err != nil {
-		return err
-	}
-	_, err = signingKey.WriteString("&")
-	if err != nil {
-		return err
-	}
-	_, err = signingKey.WriteString(url.QueryEscape(t.OauthTokenSecret))
+	// Percent encode the parameter string and append it to the output string.
+	_, err = t.BaseString.WriteString(url.QueryEscape(t.ParametersString))
 	if err != nil {
 		return err
 	}
 
-	// Getting a signing key
+	return nil
+}
+
+func (t *Twitter) createSigningKey() error {
+	var err error
+	var signingKey strings.Builder
+	// Both of these values need to be combined to form a signing key which will be used to generate the signature.
+	// The signing key is simply the percent encoded consumer secret, followed by an ampersand character ‘&’,
+	// followed by the percent encoded token secret:
+	signingKey.WriteString(url.QueryEscape(t.OauthConsumerSecret))
+	signingKey.WriteString("&")
+	signingKey.WriteString(url.QueryEscape(t.OauthTokenSecret))
+
 	hmac := hmac.New(sha1.New, []byte(signingKey.String()))
-	_, err = hmac.Write([]byte(baseString.String()))
+	fmt.Println(t.BaseString.String())
+	_, err = hmac.Write([]byte(t.BaseString.String()))
 	if err != nil {
 		return err
 	}
 
-	rawSignature := hmac.Sum(nil)
-	t.OauthSignature = base64.StdEncoding.EncodeToString(rawSignature)
+	t.SigningKey = base64.StdEncoding.EncodeToString(hmac.Sum(nil))
 
 	return nil
+
 }
 
-func (t *Twitter) createAuthHeader() error {
-	var err error
-	var headerParts []string
-	for k, v := range t.Params {
-		var p strings.Builder
-		if strings.HasPrefix(k, "oauth_") {
-			_, err = p.WriteString(url.QueryEscape(k))
-			if err != nil {
-				return err
-			}
-			_, err = p.WriteString(`="`)
-			if err != nil {
-				return err
-			}
-			_, err = p.WriteString(url.QueryEscape(v))
-			if err != nil {
-				return err
-			}
-			_, err = p.WriteString(`"`)
-			if err != nil {
-				return err
-			}
+func (t *Twitter) CreateOAuthHeader() (string, error) {
 
-			headerParts = append(headerParts, p.String())
-		}
+	var paramsString strings.Builder
+	paramsString.WriteString("OAuth ")
+
+	for k, v := range t.Parameters {
+		paramsString.WriteString(url.QueryEscape(k))
+		paramsString.WriteString("=")
+		paramsString.WriteString(url.QueryEscape(v))
+		paramsString.WriteString(", ")
 	}
-
-	t.OautHeader.Add("Authorization", "OAuth "+strings.Join(headerParts, ","))
-	return nil
-}
-
-func (t *Twitter) Request() error {
-	var err error
-	ctx := context.Background()
-	var endpoint strings.Builder
-	endpoint.WriteString(TwitterUrl)
-	endpoint.WriteString(TwitterOAuthEndpointRequestToken)
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint.String(), nil)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != 200 {
-		return errors.New(resp.Status)
-	}
-
-	resBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(resBody))
-
-	return nil
+	paramsString.WriteString("oauth_signature=")
+	paramsString.WriteString(url.QueryEscape(t.SigningKey))
+	return paramsString.String(), nil
 }
